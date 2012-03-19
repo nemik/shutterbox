fa393a60fb470e15f55361b3912b9890895c2bd9
Nemanja Stefanovic <nemik@nemik.net>
2012-03-18 20:25:54 -0500

    clean up a bit

--- shutterbox.c
+++ shutterbox.c
@@ -16,86 +16,72 @@
 
 void pulseIR(long microsecs) 
 {
-  // we'll count down from the number of microseconds we are told to wait
- 
-  while (microsecs > 0) 
+	while (microsecs > 0) 
 	{
-    // 38 kHz is about 13 microseconds high and 13 microseconds low
-   sbi(PORTB,PB4);
-	 //_delay_us(3);
-	 //digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
-   _delay_us(11);
-	 //delayMicroseconds(10);         // hang out for 10 microseconds
-   cbi(PORTB,PB4);
-	 //digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
-   _delay_us(11);
-   //delayMicroseconds(10);         // hang out for 10 microseconds
- 
-   // so 26 microseconds altogether
-   microsecs -= 26;
-  }
+		// 38 kHz is about 13 microseconds high and 13 microseconds low
+		sbi(PORTB,PB4);
+		_delay_us(11);
+		cbi(PORTB,PB4);
+		_delay_us(11);
+
+		// so 26 microseconds altogether, pin settings take a little time too
+		microsecs -= 26;
+	}
 }
 
 void SendPentaxCode() 
 {
-  //cli();  // this turns off any background interrupts
-  
-  pulseIR(12740);
-  
-  _delay_us(2920);
-  pulseIR(1000);
-  
-  _delay_us(980);
-  pulseIR(980);
-  
-  _delay_us(980);
-  pulseIR(980);
-  
-  _delay_us(980);
-  pulseIR(1000);
-  
-  _delay_us(980);
-  pulseIR(980);
-  
-  _delay_us(980);
-  pulseIR(980);
-  
-  _delay_us(980);
-  pulseIR(980);
-  
-	//sei();  // this turns them back on
+	pulseIR(12740);
+
+	_delay_us(2920);
+	pulseIR(1000);
+
+	_delay_us(980);
+	pulseIR(980);
+
+	_delay_us(980);
+	pulseIR(980);
+
+	_delay_us(980);
+	pulseIR(1000);
+
+	_delay_us(980);
+	pulseIR(980);
+
+	_delay_us(980);
+	pulseIR(980);
+
+	_delay_us(980);
+	pulseIR(980);
 }
 
 
 int main(void)
 {
-    // Set up Port B pin 4 mode to output
-    DDRB = 1<<DDB4;
- 
-    // Set up Port B data to be all low
-    PORTB = 0;  
+	// Set up Port B pin 4 mode to output
+	DDRB = 1<<DDB4;
 
-		//GIMSK = _BV (INT0); // int - Enable external interrupts int0
-		GIMSK = _BV (PCIE);
-		PCMSK = _BV (PCINT0);            // int - Enable external interrupts PCINT1
-		
-		//on rising edge
-		MCUCR = _BV (ISC01); // int - INT0 is falling edge
-		//MCUCR = _BV (ISC00); // int - INT0 is on any edge
-		
-		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	// Set up Port B data to be all low
+	PORTB = 0;  
+
+	GIMSK = _BV (PCIE);
+	PCMSK = _BV (PCINT0);            // int - Enable external interrupts PCINT1
+
+	//on falling edge
+	MCUCR = _BV (ISC01);
+
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	sei();
+
+	while (1) 
+	{
+		sleep_mode();
+		cli();
+		SendPentaxCode();
+		//ghetto debounce
+		_delay_ms (200);
 		sei();
-
-    while (1) 
-		{
-			sleep_mode();
-			cli();
-			SendPentaxCode();
-      _delay_ms (200);
-			sei();
-			//SendPentaxCode(); 
-      // Pause a little while
-    }
+	}
 }
 
 ISR (PCINT0_vect) // Interrupt on Int0 vector
